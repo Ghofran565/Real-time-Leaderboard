291808db0988eab41e84b232c2439d644332335f
Mohammad Hossein <mym25340mmm@gmail.com>
2024-09-11 23:29:27 +0330

    Updated: tournamentMd
    added status Bcause AI offred

--- server/Models/tournamentMd.js
+++ server/Models/tournamentMd.js
@@ -1,33 +1,37 @@
 import mongoose from 'mongoose';
 
 const tournamentSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: [true, 'Tournament name is required.'],
-      unique: [true, 'Tournament name is already taken.'],
-      minLength: [2, 'Tournament name must have at least 2 characters.'],
-      maxLength: [32, 'Tournament name must not exceed 32 characters.'],
-    },
-    gameId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Game',
-      required: [true, 'Game ID is required.'],
-    },
-    prize: {
-      type: Array,
-      default:[]
-    },
-    startTime: {
-      type: String,
-      required: [true, 'Start time is required.'],
-    },
-    endTime: {
-      type: String,
-      required: [true, 'End time is required.'],
-    },
-  },
-  { timestamps: true }
+	{
+		name: {
+			type: String,
+			required: [true, 'Tournament name is required.'],
+			unique: [true, 'Tournament name is already taken.'],
+			minLength: [2, 'Tournament name must have at least 2 characters.'],
+			maxLength: [32, 'Tournament name must not exceed 32 characters.'],
+		},
+		gameId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Game',
+			required: [true, 'Game ID is required.'],
+		},
+		prize: {
+			type: Array,
+			default: [],
+		},
+		startTime: {
+			type: String,
+			required: [true, 'Start time is required.'],
+		},
+		endTime: {
+			type: String,
+			required: [true, 'End time is required.'],
+		},
+		status: {
+			type: String,
+			default: 'not-active', //TODO Cns have to be checked Bcause of that
+		},
+	},
+	{ timestamps: true }
 );
 
 const Tournament = mongoose.model('Tournament', tournamentSchema);
